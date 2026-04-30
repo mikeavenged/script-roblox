a1cf1df316f5d6d713485704cedd788e7eeab592
mikeavenged <nonbig0304@gmail.com>
2026-04-30 20:18:00 +0700

    Update smallhit.lua

--- smallhit.lua
+++ smallhit.lua
@@ -315,7 +315,7 @@ task.spawn(function()
 
                     if _G.Hitbox then
                         -- เปิด Hitbox
-                        root.Size = Vector3.new(50,50,50)
+                        root.Size = Vector3.new(20,20,20)
                         root.Transparency = 0.6
                         root.CanCollide = false
                         root.Massless = true
